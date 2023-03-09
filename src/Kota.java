1b4d33601ba6839c67fbb5d6c69c4971174fdc81
euphorida <127375994+euphorida@users.noreply.github.com>
2023-03-09 20:21:58 +0700

    Add files via upload

---
+++ src/Kota.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Kota<E>{
+    private E element;
+
+    public Kota(E kota){
+        element=kota;
+    }
+
+    public E getElement(){
+        return element;
+    }
+
+    public static void main(String[] args){
+        Kota<Integer> jumlahKota = new Kota<Integer>(9);
+        Kota<String> namaKota = new Kota<String>("Malang");
+        System.out.println("Jumlah kota di Jawa Timur : "+ jumlahKota.getElement() + " kota");
+        System.out.println("Salah satu kota di Jawa Timur : Kota "+ namaKota.getElement());
+    }
+}
+
+
+
+
+
+
---
+++ src/Minuman.java
@@ -0,0 +1,6 @@
+public class Minuman extends Hidangan{
+    public String disantap(){
+        return this.getNamaHidangan() + " diminum";
+    }
+}
+
